00d333b3393d86fb9d676a9430dcc65d758a009b
Shoaib2545 <md.shoaib2545@gmail.com>
2022-11-02 18:35:50 +0500

    Add files via upload

---
+++ web_technologies_dotnet/class_03_02_nov_2022/task_4/app.js
@@ -0,0 +1,17 @@
+function chk() {
+    var a = document.getElementById("inp1").value;
+    var spaceCount = 0;
+    for (var i = 0; i < a.length; i++) {
+        var b = a[i];
+        if (b == " ") {
+            spaceCount++;
+        }
+    }
+    if (a != "") {
+        document.getElementById("result").innerHTML = "Space counts: " + spaceCount;
+    } else {
+        document.getElementById("result").innerHTML = "Write something";
+    }
+
+
+}
